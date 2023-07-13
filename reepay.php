c6c325347ec38add2edb5f93ba2b01f1411a5d83
maxwhite <maxwhite1983@gmail.com>
2023-07-13 18:27:13 +0300

    increase version

--- reepay.php
+++ reepay.php
@@ -32,7 +32,7 @@ class Reepay extends PaymentModule
     {
         $this->name = 'reepay';
         $this->tab = 'payments_gateways';
-        $this->version = '1.2.2';
+        $this->version = '1.2.3';
         $this->author = 'LittleGiants';
         $this->need_instance = 0;
 
